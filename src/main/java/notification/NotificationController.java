cf0da64c3f0d5ad3721e396daedb84afa9be83f6
i11007020 <i11007020@gmail.com>
2020-07-29 17:02:23 +0800

    commit

--- src/main/java/notification/NotificationController.java
+++ src/main/java/notification/NotificationController.java
@@ -97,15 +97,14 @@ public class NotificationController {
     }
 
 
-	@FeignRequest(client = OrderingInterface.class, method = "getSomething", parameterTypes = String.class)
-	@ApiOperation(value = "拿東西", notes = "拿東西")
+	@FeignRequest(client = OrderingInterface.class, method = "getNotificationInformation", parameterTypes = String.class)
+	@ApiOperation(value = "拿資訊", notes = "拿資訊")
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value = "/getSomething", method = RequestMethod.GET)
-	public String getSomething(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
+	@RequestMapping(value = "/getNotificationInformation", method = RequestMethod.GET)
+	public String getNotificationInformation(@ApiParam(required = true, name = "userID", value = "使用者編號") @RequestParam("userID") String userID)
 	{
-		String data = "";
 
-		return orderingInterface.getSomething(userID);
+		return orderingInterface.getNotificationInformation(userID);
 
 	}
 }
